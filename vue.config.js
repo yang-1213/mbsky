36a22d334886f0ecf3edbf378dca6b06d0a6070b
yang-1213 <gy148485@163.com>
2020-09-18 18:54:48 +0800

    生产环境去除map

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
     publicPath: "/", //部署应用包时的基本 URL
     outputDir: "dist", //生产环境构建文件的目录
     assetsDir: "", //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+    productionSourceMap: false,
     pluginOptions: {
         // 自动引入全局less
         "style-resources-loader": {
